27deda6d4e3ded9f932c2954f6ab3b2d58d664d2
Tapas Mandal <alloymobileinc@gmail.com>
2023-07-24 18:45:40 -0400

    Added all the end points

--- src/main/java/com/alloymobile/college/service/StudentService.java
+++ src/main/java/com/alloymobile/college/service/StudentService.java
@@ -2,9 +2,11 @@ package com.alloymobile.college.service;
 
 import com.alloymobile.college.persistence.Student;
 import com.alloymobile.college.persistence.StudentRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class StudentService {
@@ -14,5 +16,21 @@ public class StudentService {
 
     public Flux<Student> getAllStudent(){
         return studentRepository.findAll();
+    }
+
+    public Mono<Student> addStudent(Student student){
+        student.setId(new ObjectId().toString());
+        return studentRepository.save(student);
+    }
+
+    public Mono<Student> updateStudent(String id,Student student){
+        return studentRepository.findById(id).flatMap(s->{
+            s.setName(student.getName());
+            return studentRepository.save(s);
+        });
+    }
+
+    public Mono<Void> deleteStudent(String id){
+        return studentRepository.deleteById(id);
     }
 }
--- src/main/java/com/alloymobile/college/web/StudentResource.java
+++ src/main/java/com/alloymobile/college/web/StudentResource.java
@@ -3,9 +3,9 @@ package com.alloymobile.college.web;
 import com.alloymobile.college.persistence.Student;
 import com.alloymobile.college.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class StudentResource {
@@ -17,4 +17,20 @@ public class StudentResource {
     public Flux<Student> getAllStudent(){
         return studentService.getAllStudent();
     }
+
+    @PostMapping(value = "/students", consumes = "application/json", produces = "application/json" )
+    public Mono<Student> addStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
+
+    @PutMapping(value = "/students/{id}", consumes = "application/json", produces = "application/json" )
+    public Mono<Student> updateStudent(@PathVariable String id, @RequestBody Student student){
+        return studentService.updateStudent(id,student);
+    }
+
+    @DeleteMapping(value = "/students/{id}")
+    public Mono<Void> deleteStudent(@PathVariable String id) {
+        return studentService.deleteStudent(id);
+    }
+
 }
